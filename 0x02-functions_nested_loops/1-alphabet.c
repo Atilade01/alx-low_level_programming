1c11ebf407ea9fd1fa72d79bd99fa93b91ed423d
Atilade01 <hatilade@gmail.com>
2022-09-13 19:10:38 -0700

    Printing times 10

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,12 @@
  * Description: program that prints alphabet in lowercase
  * Return: 0
  */
-
-/**
- * main - check the code
- * Return: Always 0.
- */
 void print_alphabet(void)
 {
 	int j;
 
 	for (j = 'a'; j <= 'z'; j++)
+
 	{
 		_putchar(j);
 	}
