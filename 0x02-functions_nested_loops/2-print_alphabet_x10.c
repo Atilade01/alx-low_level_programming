a8581fdf37ac93babedeaee7f10b23929933f2f3
Atilade01 <hatilade@gmail.com>
2022-09-14 10:47:03 -0700

    Printing lowercase letters 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,11 @@
 void print_alphabet_x10(void)
 {
 	char c;
+	int i;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		for (int i = 1; i <= 10; i++)
+		for (i = 1; i <= 10; i++)
 			_putchar(c);
 	}	
 	_putchar('\n');                                                                                                                         
