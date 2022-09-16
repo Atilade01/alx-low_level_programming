34507d348ff2cfb1ee6ce72ade5876dbaf3d4c1b
Atilade01 <hatilade@gmail.com>
2022-09-15 20:48:15 -0700

    Is upper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+/**
+ * Main - Header file for all function prototypes
+ * Return - return value (0)
+ */
+
+#ifndef MAIN_H                                                                                                                                  
+#define MAIN_H 
+
+int _isupper(int c);
+int _putchar(char c);
+
+#endif
