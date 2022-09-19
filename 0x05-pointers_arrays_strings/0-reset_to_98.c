f612ad91a9075dabec8da41fcb919b69384b618d
Atilade01 <hatilade@gmail.com>
2022-09-19 01:07:05 -0700

    Update on Betty style documentation included

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,6 +2,7 @@
 
 /**
  * reset_to_98 - function to take pointer to int and update its value
+ * @*n: the pointer parameter that updates the value of the variable it points to
  * RETURN - returns void
  */
 
