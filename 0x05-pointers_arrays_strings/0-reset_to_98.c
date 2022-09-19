a6b2eb986721d1ea254573ba171236c9012fccf9
Atilade01 <hatilade@gmail.com>
2022-09-19 00:59:09 -0700

    Creating the file 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to take pointer to int and update its value
+ * RETURN - returns void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98; /* Updating the value of the int to 98 */
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a directory for writing c pointers and arrays
