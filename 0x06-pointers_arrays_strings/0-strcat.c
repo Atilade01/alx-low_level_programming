703e71d88e0ff64e8be1c6e6d62c8e9c96666404
Atilade01 <hatilade@gmail.com>
2022-09-22 21:36:23 -0700

    Concatenating strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,13 +15,17 @@ char *_strcat(char *dest, char *src)
 	j = 0;
 
 	while (dest[i] != '\0')
-		i++;
+	{ i++; }
+	i = (i + 1);
+
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
+
 	dest[i] = '\0';
+
 	return (dest);
 }
