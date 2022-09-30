face2e209d92043e938ce38b1342880b0720f369
Atilade01 <hatilade@gmail.com>
2022-09-29 21:53:22 -0700

    Advanced commit 2

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,6 @@
-substring_match - check if a substring after wildcard matches s1
+#include "main.h"
+/** 
+ * substring_match - check if a substring after wildcard matches s1
  * @s1: one string
  *  * @s2: one string
  *   * @after_wldcd: placeholder for position right after wildcard
